aa3944cff247b2d7670e906b4e82b8708a26da5e
finn cullen <finncullen83@gmail.com>
2026-09-22 12:01:14 +0900

    gamestatemanager now checks when activeNodes is empty and swaps scene. also when stopallsources is called every medium is set to a high decay medium to speed up transition.

--- Assets/code/gameStateManager.cs
+++ Assets/code/gameStateManager.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Jobs;
+using UnityEngine.SceneManagement;
 
 public class gameStateManager : MonoBehaviour
 {
 
+    mesh_manager mesh;
+
+    public bool transition;
+    public string NEWSCENE;
+
+    void Awake(){
+        mesh = FindObjectsByType<mesh_manager>(FindObjectsSortMode.None)[0];
+    }
 
     void Update(){
-        // DEBUG
-        if(Input.GetKeyDown(KeyCode.E)){
-            StopAllSources();
+        if(transition){
+            // we be transitioning
+            if(mesh.grid.activeNodes.Length == 0){
+                // no noise in the world
+                SceneManager.LoadScene(NEWSCENE);
+            }
         }
     }
 
@@ -24,9 +37,27 @@ public class gameStateManager : MonoBehaviour
         foreach(SoundSwitch S in s){S.enabled = false;}
         foreach(BALGOS B in b){B.enabled = false;}
 
-        //mediun medium = gameObject.AddComponent<mediun>();
-        //medium.width = -1;
-        //medium.medium_val.decay = 2;
-        //medium.medium_val.minDecibelVal = 0;
+        mat_vals MV = new mat_vals();
+        MV.decay = 2;
+        MV.maxDecibelVal = 200;
+        MV.spreadRate = 1;
+        MV.spreadPerc = 1f;
+
+        var jobInit = new InitGridJob
+        {
+            NodeArr = mesh.grid.Nodes,
+            PosArr = mesh.grid.worldPositions,
+            width = Mathf.RoundToInt(mesh.grid.width/mesh.grid.spacing),
+            spacing = mesh.grid.spacing,
+            medium_val = MV,
+            SPWidth = mesh.grid.spacingWidth,
+            GRWidth = mesh.grid.gradientWidth,
+            GRHeight = mesh.grid.gradientHeight,
+        };
+        JobHandle handleInit = jobInit.Schedule(mesh.grid.Nodes.Length, 512);
+        handleInit.Complete();
+        // sets every medium to a fast decay so we can load in a timely manner
+
+        transition = true;
     }
 }
