2db5d8a7c6c5cd18013e3f3d9e18e0a4e4524a14
finn cullen <finncullen83@gmail.com>
2026-09-26 19:17:22 +0900

    added checklist function to navigator class, checks over list of positions and sees if any pass over the goal or double back.

--- Assets/code/navigator.cs
+++ Assets/code/navigator.cs
@@ -13,6 +13,8 @@ public class navigator : MonoBehaviour
     public Transform tarPos;
     mesh_manager mesh;
 
+    public float targetRad;
+
     void Start(){
         mesh = FindObjectsByType<mesh_manager>(FindObjectsSortMode.None)[0];
     }
@@ -24,14 +26,58 @@ public class navigator : MonoBehaviour
         }
     }
 
+    public List<float2> posList;
+
     public void Navigate(){
-        // order of conduct
-        // establish init perameters for search
-        // generate positions
-        //      generate prongs at position
-        //      discover most legitimate prong
-        //      edit prong pos to be better (in line with center of room / tarpos)
-        generatePositions();
+        bool goal = false;
+        while(!goal){
+            // checks list so far, removes redundant moves, checks if goal is reached
+            goal = checkList();
+        }
+    }
+
+    bool checkList(){
+        bool ch = false;
+        // checks list, identifies if we are at goal or not
+        if(posList.Count > 1){
+            // does the check
+            for(int i = 1; i < posList.Count; i++){
+                float2 PosA = posList[i];
+                float2 PosB = posList[i-1];
+                bool checkPath = PosA.x < tarPos.position.x && PosB.x > tarPos.position.x;
+                checkPath = checkPath || (PosA.x > tarPos.position.x && PosB.x < tarPos.position.x);
+                float chVar = Mathf.Abs(PosA.y-tarPos.position.y);
+                if(Mathf.Abs(PosA.x-PosB.x) < 0.5f){
+                    checkPath = PosA.y < tarPos.position.y && PosB.y > tarPos.position.y;
+                    checkPath = checkPath || (PosA.y > tarPos.position.y && PosB.y < tarPos.position.y);
+                    chVar = Mathf.Abs(PosA.x-tarPos.position.x);
+                }
+                // checkPath checks if the target position lies between the nodes
+                // chVar is the distance from the travel directio of the nodes and the target pos
+                if(chVar < targetRad && checkPath){
+                    Debug.Log("correct");
+                    ch = true;
+                }
+                if(i > 1){
+                    // from the third node onward we check for double backs
+                    float2 PosC = posList[i-2];
+                    string AtoB = "across";
+                    string BtoC = "across";
+                    if(Mathf.Abs(PosA.x-PosB.x) < 0.5f){AtoB = "above";}
+                    if(Mathf.Abs(PosB.x-PosC.x) < 0.5f){BtoC = "above";}
+                    if(AtoB == BtoC){
+                        // checks if the nodes double back (I.E same direction is used twice in succession) and handles it
+                        Debug.Log("removed double back");
+                        posList.RemoveAt(i-1);
+                        i--;
+                    }
+                }
+            }
+        }
+        else{
+            posList.Add((Vector2)transform.position);
+        }
+        return ch;
     }
 
     public void generatePositions(){
